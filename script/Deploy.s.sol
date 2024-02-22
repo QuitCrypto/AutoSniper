04d855224cb96643f17bff916ae2d023f1bf0f13
0xQuit <quitcrypto@gmail.com>
2024-02-22 13:40:17 -0800

    Add deployer from

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -12,11 +12,11 @@ interface ImmutableCreate2Factory {
 contract Deploy is Script {
     ImmutableCreate2Factory immutable factory = ImmutableCreate2Factory(0x0000000000FFe8B47B3e2130213B802212439497);
     bytes initCode = type(AutoSniper).creationCode;
-    bytes32 salt = 0xd6309d958328352620b8856c0b08bd64c82244d2b49de99ef3aae416203e5433;
+    bytes32 salt = 0xd6309d958328352620b8856c0b08bd64c82244d2acc4c2380244e509848dd111;
 
     function run() external {
         console2.logBytes32(keccak256(initCode));
-        vm.startBroadcast();
+        vm.startBroadcast(0xD6309D958328352620B8856c0b08Bd64C82244d2);
 
         address sniperAddress = factory.safeCreate2(salt, initCode);
         AutoSniper sniper = AutoSniper(payable(sniperAddress));
